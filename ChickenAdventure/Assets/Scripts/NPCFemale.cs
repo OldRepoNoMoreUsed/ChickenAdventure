fc9a9636ddcaad0521bc699e59011ecf3221d990
Cyril Ruedin <Cyril Ruedin>
2016-09-06 21:56:52 +0200

    Conversation bouton OK

--- ChickenAdventure/Assets/Scripts/NPCFemale.cs
+++ ChickenAdventure/Assets/Scripts/NPCFemale.cs
@@ -4,15 +4,47 @@ using UnityEngine.UI;
 
 public class NPCFemale : Items {
 
-	Canvas CanvasConv;
+	public Sprite Face;
+	private Text NPCText;
+
+	private UI Ui;
 
 	// Use this for initialization
 	void Start () {
+
 		Id = 4;
 	}
-	
-	// Update is called once per frame
+
 	public void OnUse () {
-		//conv = GameObject.Find ("CanvasConv").GetComponent<Canvas> ();
+		print ("usususus");
+
+		Ui = GameObject.Find ("Canvas").GetComponent<UI>();
+		Ui.StartConversation (this, Face);
+		Ui.SaySomething ("Salut, je m'appelle Ivonne");
+		Ui.SetAnswers ("Fais moi un Sandwich !!", "Taguleueeleu", "Combien la passe ?");
+
+		/*
+		NPCText = GameObject.Find ("NPCText").GetComponent<Text> ();
+		NPCText.enabled = true;
+		NPCText.text = "Je suis une connasse";
+		*/
+	}
+
+	public void Answer(int R){
+		switch (R) {
+			case 0:
+				Ui.SaySomething ("Oui, je suis une femme, je suis là pour ça");
+				break;
+			case 1:
+				Ui.SaySomething ("Mais toi ferme ta gueule enkulasse de pute");
+				break;
+			case 2:
+				Ui.SaySomething ("Va te faire ta mère plutot");
+				break;
+			default:
+				Ui.CloseConversation ();
+				break;
+		}
+        Ui.SetAnswers("...", "...", "Adieu");
 	}
 }
---
+++ ChickenAdventure/Assets/Scripts/UI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	public Text textMP;
+	public Text textHP;
+
+	public Image NPCFace;
+	public Text NPCText;
+
+	public Image ConvBack;
+
+	public Button Choice1;
+	public Button Choice2;
+	public Button Choice3;
+
+    public Player Player;
+
+	NPCFemale Npc;
+
+	public Canvas CanvasConv;
+
+	void Awake () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	public void StartConversation(NPCFemale Npc, Sprite Face){
+		this.Npc = Npc;
+		CanvasConv.enabled = true;
+		NPCFace.sprite = Face;
+	}
+
+	public void SaySomething(string Said){
+		NPCText.text = Said;
+	}
+
+	public void SetAnswers (string A, string B, string C){
+		Choice1.GetComponentInChildren<Text> ().text = A;
+		Choice2.GetComponentInChildren<Text> ().text = B;
+		Choice3.GetComponentInChildren<Text> ().text = C;
+	}
+
+	public void Answer(int Btn){
+		print ("Btn");
+		print (Btn);
+		Npc.Answer (Btn);
+	}
+	public void CloseConversation(){
+		CanvasConv.enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		CanvasConv.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
