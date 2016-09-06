00a9e63f44522a43d77dfe74c3dc133911bdd61b
cyril.ruedin <cyril.ruedin@he-arc.ch>
2016-09-06 11:23:55 +0200

    Conversation

---
+++ ChickenAdventure/Assets/Scripts/NPCFemale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NPCFemale : Items {
+
+	Canvas CanvasConv;
+
+	// Use this for initialization
+	void Start () {
+		Id = 4;
+	}
+	
+	// Update is called once per frame
+	public void OnUse () {
+		//conv = GameObject.Find ("CanvasConv").GetComponent<Canvas> ();
+	}
+}
